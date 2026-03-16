203dd086916758186181a71f9002254fb03d051c
smenterprisessrilanka <smenterprisessrilanka@gmail.com>
2026-03-16 23:56:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,8 +69,27 @@ function displayProducts(products) {
 
       <p>Rs. ${product.price}</p>
 
-      <button type="button" ${isOutOfStock ? "disabled" : ""}>
-        ${isOutOfStock ? "Unavailable" : "Add to Cart"}
+      <button class="add-to-cart-btn" type="button" ${isOutOfStock ? "disabled" : ""}>
+
+        ${isOutOfStock ? "Unavailable" : `
+          <svg xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round">
+
+          <circle cx="9" cy="21" r="1"></circle>
+          <circle cx="20" cy="21" r="1"></circle>
+          <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+
+          </svg>
+
+        Add to Cart
+       `}
       </button>
     `;
 
